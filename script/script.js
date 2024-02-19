98d6a31ce015bfb726e79ff1ade9db77139b96d1
FloreDenis2001 <floredenis907@yahoo.com>
2024-02-20 01:18:48 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,4 @@
+function toggleDropdown() {
+  var dropdownContent = document.querySelector('.header__category__dropdown__content');
+  dropdownContent.style.display = (dropdownContent.style.display === 'block') ? null : 'block';
+}
