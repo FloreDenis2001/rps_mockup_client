629971c6f6d4c2ef71fc3ab9474cc9f7ff9ecc9d
FloreDenis2001 <floredenis907@yahoo.com>
2024-02-20 16:07:12 +0200

    first commit

--- script/script.js
+++ script/script.js
@@ -2,3 +2,20 @@ function toggleDropdown() {
   var dropdownContent = document.querySelector('.header__category__dropdown__content');
   dropdownContent.style.display = (dropdownContent.style.display === 'block') ? null : 'block';
 }
+
+
+function toggleDropdownMobile() {
+  var dropdownContent = document.getElementById("dropdownContent");
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+  }
+}
+
+window.onclick = function(event) {
+  var dropdownContent = document.getElementById("dropdownContent");
+  if (dropdownContent.style.display === "block" && !event.target.matches('.headerMobile__dropdown__content')) {
+    dropdownContent.style.display = "none";
+  }
+};
